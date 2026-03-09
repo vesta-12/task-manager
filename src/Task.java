af9b8c2788eb403f76171bb82d7da4f153d1d82b
vesta-12 <sofiya.serbina@narxoz.kz>
2026-03-09 22:41:25 +0500

    feat: add task

---
+++ src/Task.java
@@ -0,0 +1,42 @@
+public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private String status;
+
+    public Task(int id, String title, String description, String status) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String toFileString() {
+        return id + "," + title + "," + description + "," + status;
+    }
+
+    @Override
+    public String toString() {
+        return id + ". " + title + " (" + description + ") - " + status;
+    }
+}
